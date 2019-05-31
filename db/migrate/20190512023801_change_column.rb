b1621dcd14d28408f8aea5979715bcbc05e4b79a
brandnamewater <brandnamewater@gmail.com>
2019-05-31 18:21:20 -0400

    adsf

--- db/migrate/20190512023801_change_column.rb
+++ db/migrate/20190512023801_change_column.rb
@@ -1,5 +1,5 @@
 class ChangeColumn < ActiveRecord::Migration[5.2]
   def change
-    change_column :store_products, :store_product_id, :integer, limit: 8
+    change_column :store_products, :store_product_id, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -93,7 +93,7 @@ ActiveRecord::Schema.define(version: 2019_05_25_190842) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "product_id"
-    t.bigint "store_product_id"
+    t.string "store_product_id"
     t.integer "shop_id"
   end
 
@@ -101,30 +101,6 @@ ActiveRecord::Schema.define(version: 2019_05_25_190842) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "line_items", "carts"
