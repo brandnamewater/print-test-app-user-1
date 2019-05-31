e17bb7c59124d84ac9a632c2f9e666294a75482f
brandnamewater <brandnamewater@gmail.com>
2019-05-31 19:35:34 -0400

    asfd

--- app/controllers/shopify_app/authenticated_controller.rb
+++ app/controllers/shopify_app/authenticated_controller.rb
@@ -7,15 +7,5 @@ module ShopifyApp
     protect_from_forgery with: :exception
     before_action :login_again_if_different_shop
     around_action :shopify_session
-
-    helper_method :current_user
-
-   def current_user
-     if session[:user_id]
-       @current_user ||= User.find(session[:user_id])
-     else
-       @current_user = nil
-     end
-   end
   end
 end
