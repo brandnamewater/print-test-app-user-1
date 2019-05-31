7194b043535afa69dc1340966bf4bc81d7132def
brandnamewater <brandnamewater@gmail.com>
2019-05-31 19:56:07 -0400

    asfd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,11 +25,12 @@ class UsersController < ShopifyApp::AuthenticatedController
   # POST /users.json
   def create
     @user = User.new(user_params)
+    shop = Shop.find_by(shopify_domain: shop_domain)
+    shop.update(user_id: @user.id)
 
     respond_to do |format|
       if @user.save
-        shop = Shop.find_by(shopify_domain: shop_domain)
-        shop.update(user_id: @user.id)
+
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
