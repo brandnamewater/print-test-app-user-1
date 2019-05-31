06f438e8b23a785e0b5edb95654e02533bd81b23
brandnamewater <brandnamewater@gmail.com>
2019-05-31 19:32:27 -0400

    asfd

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ShopifyApp::AuthenticatedController
+  helper_method :current_user
+
+ def current_user
+   if session[:user_id]
+     @current_user ||= User.find(session[:user_id])
+   else
+     @current_user = nil
+   end
+ end
+ 
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
