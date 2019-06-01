8cdcad73f81f14bd20b2039e565986430f9e2b74
brandnamewater <brandnamewater@gmail.com>
2019-05-31 20:07:11 -0400

    asfd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ShopifyApp::AuthenticatedController
   # POST /users.json
   def create
     @user = User.new(user_params)
-    shop = Shop.find_by(shopify_domain: shop_domain)
+    shop = Shop.find_by(params[:shop_id])
     shop.update(user_id: @user.id)
 
     respond_to do |format|
